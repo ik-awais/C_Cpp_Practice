9b170bb7b6640bbec9d1de706693250c698a5013
ik-awais <mawaisqq@gmail.com>
2025-11-07 01:22:51 +0500

    Updates

--- C/Arrays_1D/Array4.c
+++ C/Arrays_1D/Array4.c
@@ -1,5 +1,23 @@
+//Practice problems from Starting out with C++ by Tony Gaddis
 #include <stdio.h>
+#define CONST 20
+
 int main() {
-    
+
+    int names[CONST];
+    int sum;
+
+    for (int i = 0; i < CONST; i++) {
+        printf("Enter num %d: ", i + 1);
+        scanf("%d", &names[i]);
+        sum += names[i];
+    }    
+    printf("\n");
+    for (int i = 0; i < CONST; i++) {
+        printf("%d ", names[i]);
+    }
+
+    printf("Sum: %d\n", sum);
+
     return 0;
 }
