81508c04d23e7b5e9f3af1ae00f7838ce23a7365
ik-awais <mawaisqq@gmail.com>
2026-01-21 12:47:28 +0500

    Added cpp file

---
+++ C++/functions.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void findFactors(int num);
+int findGCD(int num, int num2);
+
+int main(){
+    int num = 24, num2 = 20;
+    string lcm = "LCM is:";
+    findFactors(num);
+    findFactors(num2);
+    cout << setw(9) << lcm;
+    cout << setw(4) << num*num2/findGCD(num, num2) << endl;
+    return 0;
+}
+void findFactors(int num){
+    //Finding factors of num
+    for(int i = 2; i <= num; i++)
+    {
+        if(num % i == 0)
+        {
+            cout << setw(3) << i;
+        }
+    }
+    cout << "\n";
+}
+int findGCD(int num, int num2){
+    //Finding GCD
+    int gcd = 0;
+    for(int i = 2; i < num || i < num2; i++)
+    {
+        if(num % i == 0 && num2 % i == 0)
+        {
+            if(gcd < i)
+            {
+                gcd = i;
+            }
+        }
+    }
+    return gcd;
+}
